143bd1bf7f448fe0ed4933349e13ac384e87512f
Bhagirath Bhardwaj <bhagirathbhard@users.noreply.github.com>
2018-12-09 15:41:02 -0500

    Authentication File

---
+++ config.py
@@ -0,0 +1,8 @@
+#seperate place to store the Oauth information for the bot to use.
+
+username = 'RedDefineBot'
+password = 'REDACTED'
+client_secret = 'REDACTED'
+client_id = 'REDACTED'
+
+#it is important to not upload this file on github / share it with anyone. If you want to post it, redact all the revealing information to better protect account details.

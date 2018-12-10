dc12f3dfdd54527aeff9b10bafbdff3264cb09ff
Bhagirath Bhardwaj <bhagirathbhard@users.noreply.github.com>
2018-12-09 23:26:43 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 #seperate place to store the Oauth information for the bot to use.
 
 username = 'RedDefineBot'
-password = 'REDACTED'
-client_secret = 'REDACTED'
-client_id = 'REDACTED'
+password = os.environ['reddit_password']
+client_secret = os.environ['clients']
+client_id = os.environ['clientid']
 
 #it is important to not upload this file on github / share it with anyone. If you want to post it, redact all the revealing information to better protect account details.
